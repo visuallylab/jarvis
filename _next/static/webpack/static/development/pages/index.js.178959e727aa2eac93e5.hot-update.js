924aee8d14c4b2167d2984daa7d375a2c5206234
TseHang <a54383813@gmail.com>
2019-07-09 21:19:42 +0800

    Updates

---
+++ _next/static/webpack/static/development/pages/index.js.178959e727aa2eac93e5.hot-update.js
@@ -0,0 +1,123 @@
+webpackHotUpdate("static/development/pages/index.js",{
+
+/***/ "./components/Jarvis/Listening.tsx":
+/*!*****************************************!*\
+  !*** ./components/Jarvis/Listening.tsx ***!
+  \*****************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _babel_runtime_corejs2_regenerator__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @babel/runtime-corejs2/regenerator */ "./node_modules/@babel/runtime-corejs2/regenerator/index.js");
+/* harmony import */ var _babel_runtime_corejs2_regenerator__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_babel_runtime_corejs2_regenerator__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var _babel_runtime_corejs2_helpers_esm_asyncToGenerator__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! @babel/runtime-corejs2/helpers/esm/asyncToGenerator */ "./node_modules/@babel/runtime-corejs2/helpers/esm/asyncToGenerator.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_2__);
+/* harmony import */ var styled_components__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! styled-components */ "./node_modules/styled-components/dist/styled-components.browser.esm.js");
+/* harmony import */ var react_spring__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! react-spring */ "./node_modules/react-spring/web.js");
+/* harmony import */ var siriwave__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(/*! siriwave */ "./node_modules/siriwave/dist/siriwave.m.js");
+/* harmony import */ var _contexts_jarvisContext__WEBPACK_IMPORTED_MODULE_6__ = __webpack_require__(/*! ../../contexts/jarvisContext */ "./contexts/jarvisContext.tsx");
+/* harmony import */ var _services_JarvisService__WEBPACK_IMPORTED_MODULE_7__ = __webpack_require__(/*! ../../services/JarvisService */ "./services/JarvisService.ts");
+
+
+
+
+
+
+
+
+
+var Wrapper = Object(styled_components__WEBPACK_IMPORTED_MODULE_3__["default"])(react_spring__WEBPACK_IMPORTED_MODULE_4__["animated"].div).withConfig({
+  displayName: "Listening__Wrapper",
+  componentId: "sc-5n7f4t-0"
+})(["position:absolute;top:2vh;right:2vw;width:84%;display:flex;flex-direction:column;align-items:center;border:solid 2px ", ";background:rgba(70,73,76,0.95);border-radius:", ";padding:8px;box-shadow:0px 0px 5px 2px #353535;p{margin:5px;}"], function (p) {
+  return p.theme.colors.spaceGray;
+}, function (p) {
+  return p.theme.borderRadius;
+});
+
+var ListeningJarvis = function ListeningJarvis() {
+  var wrapperRef = Object(react__WEBPACK_IMPORTED_MODULE_2__["useRef"])(null);
+  var jarvisWave = Object(react__WEBPACK_IMPORTED_MODULE_2__["useRef"])(null);
+
+  var _useContext = Object(react__WEBPACK_IMPORTED_MODULE_2__["useContext"])(_contexts_jarvisContext__WEBPACK_IMPORTED_MODULE_6__["JarvisContext"]),
+      status = _useContext.status,
+      response = _useContext.response,
+      setStatus = _useContext.setStatus;
+
+  var props = Object(react_spring__WEBPACK_IMPORTED_MODULE_4__["useSpring"])({
+    transform: "translateX(".concat(status === _services_JarvisService__WEBPACK_IMPORTED_MODULE_7__["JarvisStatus"].Idle ? '0%' : '0%', ")"),
+    onRest: function () {
+      var _onRest = Object(_babel_runtime_corejs2_helpers_esm_asyncToGenerator__WEBPACK_IMPORTED_MODULE_1__["default"])(
+      /*#__PURE__*/
+      _babel_runtime_corejs2_regenerator__WEBPACK_IMPORTED_MODULE_0___default.a.mark(function _callee() {
+        return _babel_runtime_corejs2_regenerator__WEBPACK_IMPORTED_MODULE_0___default.a.wrap(function _callee$(_context) {
+          while (1) {
+            switch (_context.prev = _context.next) {
+              case 0:
+                console.log('rest', status);
+
+                if (!(status === _services_JarvisService__WEBPACK_IMPORTED_MODULE_7__["JarvisStatus"].Active)) {
+                  _context.next = 5;
+                  break;
+                }
+
+                // when jarvis dialog show up, start listening
+                jarvisWave.current.start();
+                setStatus(_services_JarvisService__WEBPACK_IMPORTED_MODULE_7__["JarvisStatus"].Listening);
+                return _context.abrupt("return");
+
+              case 5:
+                console.log(status);
+
+                if (!(status === _services_JarvisService__WEBPACK_IMPORTED_MODULE_7__["JarvisStatus"].Idle)) {
+                  _context.next = 9;
+                  break;
+                }
+
+                jarvisWave.current.stop();
+                return _context.abrupt("return");
+
+              case 9:
+              case "end":
+                return _context.stop();
+            }
+          }
+        }, _callee);
+      }));
+
+      function onRest() {
+        return _onRest.apply(this, arguments);
+      }
+
+      return onRest;
+    }()
+  });
+  Object(react__WEBPACK_IMPORTED_MODULE_2__["useEffect"])(function () {
+    if (wrapperRef.current) {
+      jarvisWave.current = new siriwave__WEBPACK_IMPORTED_MODULE_5__["default"]({
+        container: document.getElementById('jarvis-wave'),
+        width: wrapperRef.current.offsetWidth * 0.8,
+        height: 40,
+        style: 'ios9',
+        amplitude: 3 // autostart: true,
+
+      });
+      jarvisWave.current.start();
+    }
+  }, []);
+  return react__WEBPACK_IMPORTED_MODULE_2___default.a.createElement(Wrapper, {
+    style: props,
+    ref: wrapperRef
+  }, react__WEBPACK_IMPORTED_MODULE_2___default.a.createElement("p", null, react__WEBPACK_IMPORTED_MODULE_2___default.a.createElement("b", null, "What can I help you ?...")), react__WEBPACK_IMPORTED_MODULE_2___default.a.createElement("div", {
+    id: "jarvis-wave"
+  }), react__WEBPACK_IMPORTED_MODULE_2___default.a.createElement("p", null, response.message));
+};
+
+/* harmony default export */ __webpack_exports__["default"] = (ListeningJarvis);
+
+/***/ })
+
+})
+//# sourceMappingURL=index.js.178959e727aa2eac93e5.hot-update.js.map
---
+++ _next/static/webpack/static/development/pages/index.js.5e36e0499fc32a3d82e6.hot-update.js
@@ -0,0 +1,178 @@
+webpackHotUpdate("static/development/pages/index.js",{
+
+/***/ "./services/JarvisService.ts":
+/*!***********************************!*\
+  !*** ./services/JarvisService.ts ***!
+  \***********************************/
+/*! exports provided: JarvisStatus, default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "JarvisStatus", function() { return JarvisStatus; });
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "default", function() { return JarvisService; });
+/* harmony import */ var _babel_runtime_corejs2_core_js_object_values__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @babel/runtime-corejs2/core-js/object/values */ "./node_modules/@babel/runtime-corejs2/core-js/object/values.js");
+/* harmony import */ var _babel_runtime_corejs2_core_js_object_values__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_babel_runtime_corejs2_core_js_object_values__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var _babel_runtime_corejs2_helpers_esm_classCallCheck__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! @babel/runtime-corejs2/helpers/esm/classCallCheck */ "./node_modules/@babel/runtime-corejs2/helpers/esm/classCallCheck.js");
+/* harmony import */ var _babel_runtime_corejs2_helpers_esm_createClass__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! @babel/runtime-corejs2/helpers/esm/createClass */ "./node_modules/@babel/runtime-corejs2/helpers/esm/createClass.js");
+/* harmony import */ var _babel_runtime_corejs2_helpers_esm_defineProperty__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! @babel/runtime-corejs2/helpers/esm/defineProperty */ "./node_modules/@babel/runtime-corejs2/helpers/esm/defineProperty.js");
+/* harmony import */ var lodash_debounce__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! lodash/debounce */ "./node_modules/lodash/debounce.js");
+/* harmony import */ var lodash_debounce__WEBPACK_IMPORTED_MODULE_4___default = /*#__PURE__*/__webpack_require__.n(lodash_debounce__WEBPACK_IMPORTED_MODULE_4__);
+
+
+
+
+
+var JarvisStatus;
+
+(function (JarvisStatus) {
+  JarvisStatus["Idle"] = "IDLE";
+  JarvisStatus["Active"] = "ACTIVE";
+  JarvisStatus["Listening"] = "LISTENING";
+  JarvisStatus["Recognizing"] = "RECOGNIZING";
+})(JarvisStatus || (JarvisStatus = {}));
+
+var regexp = {
+  HEY_JARVIS: /[J|T|G|D]arv/g,
+  STOP: /(thank you)|(stop)/g
+};
+var grammars = {
+  heyJarvis: "\n    #JSGF V1.0 utf-8 en;\n    grammar heyJarvis;\n\n    <hey> = /10/ hey | /0.2/ Hey | /0.2/ Hi | /0.2/ hi;\n    public <Jarvis> = /100/ Jarvis | /1/ Travis | /0/ Carlos | /0/ Bobby | /0/ drop it | /0/ Gabby | /0/ gummies;\n    <listening> = <hey>* <Jarvis>;\n  ",
+  stop: "\n    #JSGF V1.0 utf-8 en;\n    grammar stop;\n\n    <stop> = stop <Jarvis>*;\n    <thank you> = thank you <Jarvis>*;\n    "
+};
+
+// TODO:
+// [refactor]: handle if recognition is undefined
+var JarvisService =
+/*#__PURE__*/
+function () {
+  function JarvisService(props) {
+    var _this = this;
+
+    Object(_babel_runtime_corejs2_helpers_esm_classCallCheck__WEBPACK_IMPORTED_MODULE_1__["default"])(this, JarvisService);
+
+    Object(_babel_runtime_corejs2_helpers_esm_defineProperty__WEBPACK_IMPORTED_MODULE_3__["default"])(this, "props", void 0);
+
+    Object(_babel_runtime_corejs2_helpers_esm_defineProperty__WEBPACK_IMPORTED_MODULE_3__["default"])(this, "recognition", void 0);
+
+    Object(_babel_runtime_corejs2_helpers_esm_defineProperty__WEBPACK_IMPORTED_MODULE_3__["default"])(this, "onresult", lodash_debounce__WEBPACK_IMPORTED_MODULE_4___default()(function (event) {
+      var _this$props = _this.props,
+          status = _this$props.status,
+          setStatus = _this$props.setStatus,
+          setResponse = _this$props.setResponse;
+      var target = event.results[event.resultIndex]; // run before anything when matches "stop grammar",
+      // set jarvis status to "Idle"
+
+      if (regexp.STOP.exec(target[0].transcript) && target.isFinal) {
+        setStatus(JarvisStatus.Idle);
+        return;
+      }
+
+      switch (status.current) {
+        case JarvisStatus.Idle:
+          {
+            if (regexp.HEY_JARVIS.exec(target[0].transcript)) {
+              console.log('setste');
+              setStatus(JarvisStatus.Active);
+            }
+
+            break;
+          }
+
+        case JarvisStatus.Listening:
+          {
+            if (regexp.HEY_JARVIS.exec(target[0].transcript)) {
+              setStatus(JarvisStatus.Active);
+            } // listening suggestion
+
+
+            console.log('listening');
+            break;
+          }
+      }
+
+      console.log(event);
+      setResponse({
+        message: target[0].transcript,
+        confidence: target[0].confidence,
+        isFinal: target.isFinal
+      });
+    }, 100));
+
+    Object(_babel_runtime_corejs2_helpers_esm_defineProperty__WEBPACK_IMPORTED_MODULE_3__["default"])(this, "onstart", function () {
+      return _this.props.setEnabled(true);
+    });
+
+    Object(_babel_runtime_corejs2_helpers_esm_defineProperty__WEBPACK_IMPORTED_MODULE_3__["default"])(this, "onend", function () {
+      _this.props.setEnabled(false);
+
+      _this.props.setStatus(JarvisStatus.Idle);
+    });
+
+    Object(_babel_runtime_corejs2_helpers_esm_defineProperty__WEBPACK_IMPORTED_MODULE_3__["default"])(this, "onerror", function (event) {
+      console.error('error', event);
+    });
+
+    this.props = props; // @ts-ignore
+
+    var Recognition = window.SpeechRecognition || webkitSpeechRecognition;
+    this.recognition = new Recognition();
+    this.initialize();
+    this.recognition.onresult = this.onresult;
+    this.recognition.onstart = this.onstart;
+    this.recognition.onend = this.onend;
+    this.recognition.onerror = this.onerror; // default enable jarvis service
+
+    this.enable();
+    setTimeout(function () {
+      return _this.props.setStatus(JarvisStatus.Active);
+    }, 1000);
+  }
+
+  Object(_babel_runtime_corejs2_helpers_esm_createClass__WEBPACK_IMPORTED_MODULE_2__["default"])(JarvisService, [{
+    key: "initialize",
+    value: function initialize() {
+      if (this.recognition) {
+        var speechGrammarList = this.generateGrammarList();
+        this.recognition.grammars = speechGrammarList;
+        this.recognition.lang = 'en-US';
+        this.recognition.continuous = true; // continuous results are returned for each recognition
+
+        this.recognition.interimResults = true;
+      }
+    }
+  }, {
+    key: "generateGrammarList",
+    value: function generateGrammarList() {
+      var SpeechGrammarList = // @ts-ignore
+      window.SpeechGrammarList || webkitSpeechGrammarList;
+      var speechGrammarList = new SpeechGrammarList();
+
+      _babel_runtime_corejs2_core_js_object_values__WEBPACK_IMPORTED_MODULE_0___default()(grammars).forEach(function (grammar) {
+        return speechGrammarList.addFromString(grammar, 10);
+      });
+
+      return speechGrammarList;
+    } // tslint:disable
+
+  }, {
+    key: "enable",
+    value: function enable() {
+      this.recognition.start();
+    }
+  }, {
+    key: "disable",
+    value: function disable() {
+      this.recognition.stop();
+    }
+  }]);
+
+  return JarvisService;
+}();
+
+
+
+/***/ })
+
+})
+//# sourceMappingURL=index.js.5e36e0499fc32a3d82e6.hot-update.js.map
