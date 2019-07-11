5253f9f268b452489aad3ce699c5354d0d90ff83
JHIH-CING LI <jhihcing.li@gmail.com>
2019-07-11 19:31:29 +0800

    Add indicator

--- components/Traffic/Panel.tsx
+++ components/Traffic/Panel.tsx
@@ -1,4 +1,8 @@
+import React from 'react';
 import styled from 'styled-components';
+import Indicator from './Indicator';
+import { TrafficStatus, IndicatorMessage, IndicatorColor } from '@/constants';
+import Traffic from '@/pages/traffic';
 
 const Button = styled.button`
   margin: 24px 12px 12px 24px;
@@ -28,7 +32,6 @@ const Container = styled.div`
   left: 0;
   width: 100%;
   padding: 64px 64px 36px 24px;
-  background-color: rgba(255, 255, 255, 0.05);
 `;
 
 const Info = styled.p<{ large?: boolean }>`
@@ -37,6 +40,11 @@ const Info = styled.p<{ large?: boolean }>`
   margin: 8px 0;
   font-weight: 100;
   font-size: ${props => (props.large ? '48px' : '18px')};
+`;
+
+const FlexWrapper = styled.div`
+  display: flex;
+  align-items: center;
 `;
 
 type TButton = {
@@ -48,20 +56,37 @@ type TProps = {
   title: string;
   infos: string[];
   buttonConfigs: TButton[];
+  status: TrafficStatus;
 };
 
-const Panel: React.FC<TProps> = ({ title, infos, buttonConfigs }) => (
-  <Container>
-    <Info large={true}>{title}</Info>
-    {infos.map(info => (
-      <Info key={info}>{info}</Info>
-    ))}
-    {buttonConfigs.map(config => (
-      <Button key={config.text} onClick={config.onClick}>
-        {config.text}
-      </Button>
-    ))}
-  </Container>
+const Panel: React.FC<TProps> = React.memo(
+  ({ title, infos, buttonConfigs, status }) => (
+    <Container>
+      <FlexWrapper>
+        <Info large={true}>{title}</Info>
+        <Indicator
+          text={
+            status === TrafficStatus.normal
+              ? IndicatorMessage.normal
+              : IndicatorMessage.warning
+          }
+          color={
+            status === TrafficStatus.normal
+              ? IndicatorColor.normal
+              : IndicatorColor.warning
+          }
+        />
+      </FlexWrapper>
+      {infos.map(info => (
+        <Info key={info}>{info}</Info>
+      ))}
+      {buttonConfigs.map(config => (
+        <Button key={config.text} onClick={config.onClick}>
+          {config.text}
+        </Button>
+      ))}
+    </Container>
+  ),
 );
 
 export default Panel;
